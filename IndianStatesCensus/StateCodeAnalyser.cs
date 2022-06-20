08d6c39b0bb9052df3c24803128d3cb10f6cd195
ovi2410 <guneshwarifegade@gmail.com>
2022-06-20 16:31:19 +0530

    uc2 state code added

---
+++ IndianStatesCensus/StateCodeAnalyser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensus
+{
+    public class StateCodeAnalyser
+    {
+        // read only path variable
+        readonly string filePath;
+        // variable to count numberof variables
+        int numberOfRecords = 0;
+        // variable for delimeter character
+        char delimeter;
+    }
+}
