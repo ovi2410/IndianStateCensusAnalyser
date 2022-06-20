33fea18404bdab7f88cd74a17ab8282846c710fd
ovi2410 <guneshwarifegade@gmail.com>
2022-06-20 16:17:22 +0530

    uc1 with testcase 1.1-1.5 added

---
+++ IndianStatesCensus/StateCensusException.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensus
+{
+    public enum StateCensusException
+    {
+        /// <summary>
+        /// state census enuumerations for exception types
+        /// </summary>
+        wrongcsvfile, IncorrectDelimeter, InvalidHeader, fileNotFound
+    }
+}
